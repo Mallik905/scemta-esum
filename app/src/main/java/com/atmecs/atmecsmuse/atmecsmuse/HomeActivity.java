badca0fa81a43948dcc8b516b562b98fa9dbb856
Mallikarjun Patnam <mallikarjun.patnam@atmecs.com>
2019-02-20 15:35:58 +0530

    created HTTP request on launch muse monitor

--- app/src/main/java/com/atmecs/atmecsmuse/atmecsmuse/HomeActivity.java
+++ app/src/main/java/com/atmecs/atmecsmuse/atmecsmuse/HomeActivity.java
@@ -5,6 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class HomeActivity extends AppCompatActivity {
     @Override
@@ -16,11 +25,45 @@ public class HomeActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                Intent launchMuseMonitorApp = getPackageManager().getLaunchIntentForPackage(getString(R.string.muse_monitor_package));
-                startActivity(launchMuseMonitorApp);
+
+                requestSessionStart(29);
             }
         });
 
     }
+    public void requestSessionStart(int eid){
 
+        try {
+            String url = getString(R.string.sessionstart_url);
+            JSONObject reqBody = new JSONObject();
+            reqBody.put("username",eid);
+            System.out.println(reqBody.toString());
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                    (Request.Method.POST, url, reqBody, new Response.Listener<JSONObject>() {
+
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            Intent launchMuseMonitorApp = getPackageManager().getLaunchIntentForPackage(getString(R.string.muse_monitor_package));
+                            startActivity(launchMuseMonitorApp);
+                        }
+                    }, new Response.ErrorListener() {
+
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+
+                            System.out.println(error.toString());
+                            Toast.makeText(getApplicationContext(),"HTTP Error - unable to start Muse Session", Toast.LENGTH_LONG).show();
+
+                        }
+                    });
+            MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+            // Simulate network access.
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
